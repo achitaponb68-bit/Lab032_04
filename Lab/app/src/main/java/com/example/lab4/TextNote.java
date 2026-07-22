fc429d80d4940c4830aa55d67784257c271a8c69
achitaponb68-bit <achitapon.b68@psru.ac.th>
2026-07-22 09:39:39 +0700

    Update TextNote.java

--- Lab/app/src/main/java/com/example/lab4/TextNote.java
+++ Lab/app/src/main/java/com/example/lab4/TextNote.java
@@ -1,4 +1,17 @@
 package com.example.lab4;
 
-public class TextNote {
+public class TextNote extends Note {
+
+    public TextNote(String title, String content) {
+        super(title, content);
+    }
+
+    @Override
+    public String getSummary() {
+        String content = getContent();
+        String shortContent = content.length() > 30
+                ? content.substring(0, 30) + "..."
+                : content;
+        return "[" + formatDate() + "] " + getTitle() + " (ข้อความ): " + shortContent;
+    }
 }
