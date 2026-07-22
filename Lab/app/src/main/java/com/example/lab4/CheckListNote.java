b227682b233a5232958fa7212f013c8791bd8222
achitaponb68-bit <achitapon.b68@psru.ac.th>
2026-07-22 09:38:40 +0700

    Update CheckListNote.java

--- Lab/app/src/main/java/com/example/lab4/CheckListNote.java
+++ Lab/app/src/main/java/com/example/lab4/CheckListNote.java
@@ -1,4 +1,23 @@
 package com.example.lab4;
 
-public class CheckListNote {
+import java.util.ArrayList;
+import java.util.List;
+
+public class  CheckListNote extends Note {
+    private List<String> checkList;
+
+    public CheckListNote(String title, String content) {
+        super(title,content);
+        this.checkList = new ArrayList<>();
+    }
+    public List<String> getCheckList(){
+        return checkList;
+    }
+    public void setCheckList(List<String> checkList){
+        this.checkList = checkList;
+    }
+    @Override
+    public String getSummary(){
+        return "[" + formatDate() + "]" + getTitle()+ " (เช็คลิสต์): มีทั้งหมด " + checkList.size() + " รายการ";
+    }
 }
