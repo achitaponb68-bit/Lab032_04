2445f9036c4f288e044de74c05e2e82f9e7f8fea
achitaponb68-bit <achitapon.b68@psru.ac.th>
2026-07-22 09:39:10 +0700

    Update Note.java

--- Lab/app/src/main/java/com/example/lab4/Note.java
+++ Lab/app/src/main/java/com/example/lab4/Note.java
@@ -1,4 +1,42 @@
 package com.example.lab4;
 
-public class Note {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public abstract class Note {
+    //Attributes
+    private String title;
+    private String content;
+    private Date createdDate;
+
+    public Note(String title, String content){
+        this.title = title;
+        this.content = content;
+        this.createdDate = new Date();
+    }
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public String getContent(){
+        return content;
+    }
+    public void setContent(String content){
+        this.content = content;
+    }
+    public Date getCreatedDate(){
+        return createdDate;
+    }
+    public void setCreatedDate(Date createdDate){
+        this.createdDate = createdDate;
+    }
+
+    public String formatDate() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        return sdf.format(createdDate);
+    }
+    abstract String getSummary();
 }
